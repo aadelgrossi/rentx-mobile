86a15bff81e93538963003c8c3e87600f046ac16
Andre Grossi <iamandregrossi@gmail.com>
2020-11-21 18:53:06 -0300

    Update colors object and update dependent files

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export default {
+  red: '#dc1637',
+  salmon: '#FF99AA',
+  green: '#03B252',
+  white: '#fff',
+  black: '#1B1B1F',
+  lightBlack: '#29292E',
+  grayPrimary: '#47474D',
+  graySecondary: '#A0A0B2',
+  grayText: '#7A7A80',
+  grayAccent: '#AEAEB3',
+  grayLight: '#DEDEE3',
+  grayLighter: '#EBEBF0',
+  grayLightest: '#F4F5F6',
+  hoverRed: '#FDEDEF',
+  hoverGreen: '#DAF3E5'
+}
---
+++ src/styles/default.ts
@@ -0,0 +1,9 @@
+import colors from './colors'
+
+export default {
+  header: {
+    background: colors.black,
+    title: colors.white,
+    detail: colors.grayText
+  }
+}
