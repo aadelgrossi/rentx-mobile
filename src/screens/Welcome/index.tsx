6f5c188f6af7f0f699c8d2dfa2a3a73d5a31ae41
Andre Grossi <iamandregrossi@gmail.com>
2020-11-21 21:02:49 -0300

    Update welcome screen with styles

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Image } from 'react-native'
+
+import rentXSymbol from '../../assets/logo/symbol.png'
+import {
+  Container,
+  Contents,
+  Title,
+  Text,
+  Actions,
+  LogInButton,
+  SignUpButton,
+  ButtonText,
+  BackButton,
+  BackButtonText
+} from './styles'
 
 const Welcome: React.FC = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <Contents>
+        <Image source={rentXSymbol}></Image>
+        <Title>Seja {'\n'} Bem-vindo</Title>
+        <Text>O que você deseja fazer?</Text>
+
+        <Actions>
+          <LogInButton>
+            <ButtonText>Login</ButtonText>
+          </LogInButton>
+          <SignUpButton>
+            <ButtonText>Cadastre-se</ButtonText>
+          </SignUpButton>
+        </Actions>
+
+        <BackButton>
+          <BackButtonText>Voltar</BackButtonText>
+        </BackButton>
+      </Contents>
+    </Container>
+  )
 }
 
 export default Welcome
--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -1,3 +1,72 @@
-import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import styled, { css } from 'styled-components/native'
 
-export const Container = styled.View``
+import colors from '../../styles/colors'
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${colors.black};
+`
+
+export const Contents = styled.View`
+  flex: 1;
+  align-items: center;
+  margin-top: 160px;
+`
+
+export const Title = styled.Text`
+  color: ${colors.white};
+  text-align: center;
+  font-size: 40px;
+  margin-top: 160px;
+  margin-bottom: 40px;
+  font-family: 'archivo-600';
+`
+
+export const Text = styled.Text`
+  font-family: 'inter-400';
+  font-size: 16px;
+  color: ${colors.grayLight};
+`
+
+export const Actions = styled.View`
+  position: absolute;
+  bottom: 100px;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 75%;
+`
+
+export const ButtonText = styled.Text`
+  font-family: 'inter-600';
+  font-size: 16px;
+  color: ${colors.white};
+`
+
+const buttonCss = css`
+  width: 156px;
+  height: 65px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const LogInButton = styled(RectButton)`
+  ${buttonCss}
+  background-color: ${colors.red};
+`
+
+export const SignUpButton = styled(RectButton)`
+  ${buttonCss}
+  background-color: ${colors.lightBlack};
+`
+
+export const BackButton = styled(RectButton)`
+  position: absolute;
+  bottom: 60px;
+`
+
+export const BackButtonText = styled.Text`
+  font-family: 'archivo-500';
+  font-size: 15px;
+  color: ${colors.grayText};
+`
