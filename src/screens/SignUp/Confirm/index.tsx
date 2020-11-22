6e3afed8ccdbbf6ae52020a6974c7117d2d8c611
Andre Grossi <iamandregrossi@gmail.com>
2020-11-22 17:37:26 -0300

    Style confirm page

--- src/screens/SignUp/Confirm/index.tsx
+++ src/screens/SignUp/Confirm/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import { Image } from 'react-native'
 
-import { Container } from './styles'
+import doneIndicator from '../../../assets/done.png'
+import bgLogo from '../../../assets/logo/bg.png'
+import Button from '../../../components/Button'
+import { Container, BackgroundImage, Contents, Title, Text } from './styles'
 
 const Confirm: React.FC = () => {
+  const navigation = useNavigation()
   return (
     <Container>
-      <Text>Confirm</Text>
+      <BackgroundImage source={bgLogo} />
+      <Contents>
+        <Image source={doneIndicator}></Image>
+
+        <Title>Conta criada!</Title>
+        <Text>Entre agora com sua conta para aproveitar.</Text>
+        <Button
+          style={{ marginTop: 'auto' }}
+          variant
+          onPress={() => navigation.navigate('SignIn')}
+        >
+          Retornar para o login
+        </Button>
+      </Contents>
+      <StatusBar style="light" />
     </Container>
   )
 }
