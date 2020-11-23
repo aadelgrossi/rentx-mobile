799ac3fad6af1bb3e86d305a13a040ee8a00e675
Andre Grossi <iamandregrossi@gmail.com>
2020-11-23 17:08:11 -0300

    Create and style profile page

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,13 +1,71 @@
 import React from 'react'
 
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-import { Container } from './styles'
+import RentIcon from '../../components/RentIcon'
+import { useAuth } from '../../hooks/useAuth'
+import colors from '../../styles/colors'
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Contents,
+  ProfileContainer,
+  ProfilePicture,
+  SwitchPicture,
+  InfoItem,
+  InfoTitle,
+  InfoValue,
+  Separator,
+  FavoriteCarContainer,
+  CarCard,
+  UserName
+} from './styles'
 
 const Profile: React.FC = () => {
+  const { user, signOut } = useAuth()
+  const navigation = useNavigation()
   return (
     <Container>
-      <Text>Profile</Text>
+      <Header>
+        <TouchableOpacity onPress={() => navigation.navigate('ProfileInfo')}>
+          <RentIcon name="edit" color={colors.grayAccent} />
+        </TouchableOpacity>
+        <HeaderTitle>Perfil</HeaderTitle>
+        <TouchableOpacity onPress={signOut}>
+          <RentIcon name="power" color={colors.grayAccent} />
+        </TouchableOpacity>
+      </Header>
+      <Contents>
+        <ProfileContainer>
+          <ProfilePicture
+            source={{
+              uri: user.avatar.url
+            }}
+          />
+          <SwitchPicture>
+            <RentIcon name="photo" color={colors.white} />
+          </SwitchPicture>
+        </ProfileContainer>
+
+        <UserName>{[user.firstName, user.lastName].join(' ')}</UserName>
+        <InfoItem style={{ marginTop: 24 }}>
+          <InfoTitle>Agendamentos feitos</InfoTitle>
+          <InfoValue>05</InfoValue>
+        </InfoItem>
+
+        <Separator />
+
+        <FavoriteCarContainer>
+          <InfoItem style={{ marginBottom: 16 }}>
+            <InfoTitle>Carro favorito</InfoTitle>
+            <InfoValue>Utilizado 02 vezes</InfoValue>
+          </InfoItem>
+
+          <CarCard />
+        </FavoriteCarContainer>
+      </Contents>
     </Container>
   )
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,3 +1,94 @@
+import { RectButton } from 'react-native-gesture-handler'
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+import colors from '../../styles/colors'
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${colors.white};
+  flex-direction: column;
+`
+
+export const Header = styled.View`
+  background-color: ${colors.black};
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 48px 24px;
+  height: 200px;
+`
+
+export const HeaderTitle = styled.Text`
+  font-family: 'archivo-600';
+  color: ${colors.white};
+  font-size: 30px;
+  line-height: 30px;
+`
+
+export const Contents = styled.View`
+  align-items: center;
+  padding: 0 24px 24px;
+`
+
+export const ProfileContainer = styled.View``
+
+export const ProfilePicture = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+  margin-top: -90px;
+`
+
+export const SwitchPicture = styled(RectButton)`
+  position: absolute;
+  bottom: 0;
+  right: 10px;
+  background-color: ${colors.red};
+  width: 40px;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+`
+
+export const UserName = styled.Text`
+  font-family: 'archivo-600';
+  font-size: 30px;
+  color: ${colors.black};
+  width: 180px;
+  text-align: center;
+  margin-top: 16px;
+`
+
+export const InfoItem = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const InfoTitle = styled.Text`
+  color: ${colors.grayText};
+  font-family: 'inter-400';
+  font-size: 15px;
+`
+
+export const InfoValue = styled.Text`
+  color: ${colors.black};
+  font-family: 'archivo-600';
+`
+
+export const Separator = styled.View`
+  height: 1px;
+  width: 100%;
+  margin: 16px 0;
+  background-color: ${colors.grayLighter};
+`
+
+export const FavoriteCarContainer = styled.View`
+  width: 100%;
+  justify-content: space-between;
+`
+
+export const CarCard = styled.View`
+  height: 128px;
+  background-color: ${colors.grayLight};
+`
