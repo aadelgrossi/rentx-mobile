c65f338a9c7c641da71505a7732c3b9c4dacf8ae
Andre Grossi <iamandregrossi@gmail.com>
2020-11-25 18:18:29 -0300

    Update allowed icon values, add interfaces for Car object

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,13 +3,14 @@ declare module '*.png'
 type RentIcons =
   | 'speed'
   | 'transmission'
-  | 'up'
+  | 'acceleration'
   | 'calendar'
   | 'car'
   | 'electric'
   | 'email'
   | 'gas'
   | 'home'
+  | 'hp'
   | 'hybrid'
   | 'lock'
   | 'person'
@@ -19,3 +20,30 @@ type RentIcons =
   | 'power'
   | 'eye'
   | 'eye-cross'
+  | 'search'
+
+type CustomCarSpec = 'speed' | 'acceleration' | 'hp' | 'person'
+
+interface CarSpec {
+  id: string
+  name: CustomCarSpec
+  value: string
+}
+
+interface Image {
+  id: string
+  url: string
+}
+
+interface Car {
+  id: string
+  name: string
+  manufacturer: {
+    name: string
+  }
+  fuelType: 'gas' | 'electric' | 'hybrid'
+  transmission: 'AUTO' | 'MANUAL'
+  specifications: CustomCarSpec[]
+  dailyValue: number
+  photo: Image
+}
