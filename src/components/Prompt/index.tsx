5e803ec39de16ac6b9afd8697029c141903beaf3
Andre Grossi <iamandregrossi@gmail.com>
2020-11-28 18:38:15 -0300

    Create component and add quit image

---
+++ src/components/Prompt/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { StatusBar } from 'expo-status-bar'
+import { Image } from 'react-native'
+
+import doneIndicator from '../../../assets/done.png'
+import bgLogo from '../../../assets/logo/bg.png'
+import quitIndicator from '../../../assets/quit.png'
+import { Container, BackgroundImage, Contents, Title, Text } from './styles'
+
+interface PromptProps {
+  type: 'done' | 'quit'
+  title: string
+  content: string
+}
+
+const Prompt: React.FC<PromptProps> = ({ title, content, type, children }) => {
+  return (
+    <Container>
+      <BackgroundImage source={bgLogo} />
+      <Contents>
+        <Image source={type === 'done' ? doneIndicator : quitIndicator}></Image>
+
+        <Title>{title}</Title>
+        <Text>{content}</Text>
+        {children}
+      </Contents>
+      <StatusBar style="light" />
+    </Container>
+  )
+}
+
+export default Prompt
