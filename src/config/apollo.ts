0be7d6bc4923034b3f98d7aedc64b0fce6f2799f
Andre Grossi <iamandregrossi@gmail.com>
2021-01-05 19:57:52 -0300

    Configure auth header forwarding on request upon finding it in async storage

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,22 +1,40 @@
 import {
   ApolloClient,
+  ApolloLink,
+  HttpLink,
   InMemoryCache,
   NormalizedCacheObject
 } from '@apollo/client'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const httpLink = new HttpLink({ uri: 'http://192.168.0.153:3333/graphql' })
+
+const authLink = new ApolloLink((operation, forward) => {
+  AsyncStorage.getItem('@RentX:token').then(token => {
+    console.log('trying to get token. got', token)
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  })
+
+  return forward(operation)
+})
 
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getApolloClient = () => {
+function getApolloClient(): ApolloClient<NormalizedCacheObject> {
   if (apolloClient) {
     return apolloClient
   }
 
   apolloClient = new ApolloClient({
     cache: new InMemoryCache(),
-
-    uri: 'http://192.168.0.153:3333/graphql'
+    link: authLink.concat(httpLink)
   })
 
   return apolloClient
 }
+
+export { getApolloClient }
