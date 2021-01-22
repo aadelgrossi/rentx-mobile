a4e743aaabe59f8217538d1c6438185f636d5026
Andre Grossi <iamandregrossi@gmail.com>
2021-01-22 00:23:18 -0300

    Discard useQuery use in ProfileSummary, all details can now be retrieved from useAuth

--- src/graphql/user.ts
+++ src/graphql/user.ts
@@ -3,7 +3,13 @@ import { gql } from '@apollo/client'
 export const USER_INFO = gql`
   query UserInfo {
     me {
+      name
+      email
       totalRentals
+      createdAt
+      avatar {
+        url
+      }
       favoriteCar {
         totalDays
         timesRented
@@ -16,6 +22,40 @@ export const USER_INFO = gql`
           url
         }
       }
+    }
+  }
+`
+
+export const UPDATE_USER_INFO = gql`
+  mutation updateUserInfo($data: UpdateUserInput!) {
+    updateUser(data: $data) {
+      name
+      email
+      totalRentals
+      createdAt
+      avatar {
+        url
+      }
+      favoriteCar {
+        totalDays
+        timesRented
+        model
+        dailyRate
+        manufacturer {
+          name
+        }
+        photo {
+          url
+        }
+      }
+    }
+  }
+`
+
+export const CHANGE_USER_PASSWORD = gql`
+  mutation changeUserPassword($data: ChangePasswordInput!) {
+    changePassword(data: $data) {
+      id
     }
   }
 `
